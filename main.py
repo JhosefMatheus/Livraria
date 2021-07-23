9ff377e2fb3d6e9dce379a2bb80c88b867c56e8d
JhosefMatheus <jhosef.mat123@hotmail.com>
2021-07-22 22:02:01 -0300

    alterações feitas: adicionando o promeiro design

--- main.py
+++ main.py
@@ -23,14 +23,14 @@ style.map(
 )
 
 tree_frame = Frame(root)
-tree_frame.pack(pady=10)
+tree_frame.pack(pady=10, padx=10, expand=True, fill=BOTH, anchor=N)
 
 tree_scrool = Scrollbar(tree_frame)
 tree_scrool.pack(side=RIGHT, fill=Y)
 
 my_tree = ttk.Treeview(
     tree_frame, yscrollcommand=tree_scrool.set, selectmode='extended')
-my_tree.pack(fill='both')
+my_tree.pack(expand=True, fill=BOTH)
 
 tree_scrool.config(command=my_tree.yview)
 
@@ -63,7 +63,37 @@ livros = [
     [4, 'Atos dos Apóstolos', 'Ellen G. White',
         'CPB - Casa Publicadora Brasileira', 500, 'João Artur do Nascimento Neto'],
     [5, 'O Grande Conflito', 'Ellen G. White',
-        'CPB - Casa Publicadora Brasileira', 623, 'João Artur do Nascimento Neto']
+        'CPB - Casa Publicadora Brasileira', 623, 'João Artur do Nascimento Neto'],
+    [6, 'Patriarcas e Profetas', 'Ellen G. White',
+        'CPB - Casa Publicadora Brasileira', 773, 'João Artur do Nascimento Neto'],
+    [7, 'Profetas e Reis', 'Ellen G. White',
+        'CPB - Casa Publicadora Brasileira', 500, 'João Artur do Nascimento Neto'],
+    [8, 'O Desejado de Todas as Nações', 'Ellen G. White',
+        'CPB - Casa Publicadora Brasileira', 829, 'João Artur do Nascimento Neto'],
+    [9, 'Atos dos Apóstolos', 'Ellen G. White',
+        'CPB - Casa Publicadora Brasileira', 500, 'João Artur do Nascimento Neto'],
+    [10, 'O Grande Conflito', 'Ellen G. White',
+        'CPB - Casa Publicadora Brasileira', 623, 'João Artur do Nascimento Neto'],
+    [11, 'Patriarcas e Profetas', 'Ellen G. White',
+        'CPB - Casa Publicadora Brasileira', 773, 'João Artur do Nascimento Neto'],
+    [12, 'Profetas e Reis', 'Ellen G. White',
+        'CPB - Casa Publicadora Brasileira', 500, 'João Artur do Nascimento Neto'],
+    [13, 'O Desejado de Todas as Nações', 'Ellen G. White',
+        'CPB - Casa Publicadora Brasileira', 829, 'João Artur do Nascimento Neto'],
+    [14, 'Atos dos Apóstolos', 'Ellen G. White',
+        'CPB - Casa Publicadora Brasileira', 500, 'João Artur do Nascimento Neto'],
+    [15, 'O Grande Conflito', 'Ellen G. White',
+        'CPB - Casa Publicadora Brasileira', 623, 'João Artur do Nascimento Neto'],
+    [16, 'Patriarcas e Profetas', 'Ellen G. White',
+        'CPB - Casa Publicadora Brasileira', 773, 'João Artur do Nascimento Neto'],
+    [17, 'Profetas e Reis', 'Ellen G. White',
+        'CPB - Casa Publicadora Brasileira', 500, 'João Artur do Nascimento Neto'],
+    [18, 'O Desejado de Todas as Nações', 'Ellen G. White',
+        'CPB - Casa Publicadora Brasileira', 829, 'João Artur do Nascimento Neto'],
+    [19, 'Atos dos Apóstolos', 'Ellen G. White',
+        'CPB - Casa Publicadora Brasileira', 500, 'João Artur do Nascimento Neto'],
+    [20, 'O Grande Conflito', 'Ellen G. White',
+        'CPB - Casa Publicadora Brasileira', 623, 'João Artur do Nascimento Neto'],
 ]
 
 my_tree.tag_configure('oddrow', background='white')
@@ -84,5 +114,62 @@ for livro in livros:
         my_tree.insert(parent='', index='end', values=(
             id, titulo, autor, editora, n_pags, proprietario), tags=('oddrow',))
 
+register_frame = LabelFrame(
+    root,
+    text='Adicionar Livro'
+)
+register_frame.pack(expand=True, fill='both', padx=10, pady=10, anchor=N)
+
+title_book_label = Label(register_frame, text='Título')
+title_book_label.grid(row=0, column=0, padx=10, pady=10)
+title_book_entry = Entry(register_frame)
+title_book_entry.grid(row=0, column=1, padx=10, pady=10)
+
+author_book_label = Label(register_frame, text='Autor')
+author_book_label.grid(row=0, column=2, padx=10, pady=10)
+author_book_entry = Entry(register_frame)
+author_book_entry.grid(row=0, column=3, padx=10, pady=10)
+
+publishing_company_label = Label(register_frame, text='Editora')
+publishing_company_label.grid(row=0, column=4, padx=10, pady=10)
+publishing_company_entry = Entry(register_frame)
+publishing_company_entry.grid(row=0, column=5, padx=10, pady=10)
+
+n_pages_label = Label(register_frame, text='Número de páginas')
+n_pages_label.grid(row=1, column=0, padx=10, pady=10)
+n_pages_entry = Entry(register_frame)
+n_pages_entry.grid(row=1, column=1, padx=10, pady=10)
+
+owner_book_label = Label(register_frame, text='Proprietário')
+owner_book_label.grid(row=1, column=2, padx=10, pady=10)
+owner_book_entry = Entry(register_frame)
+owner_book_entry.grid(row=1, column=3, padx=10, pady=10)
+
+button_frame = LabelFrame(root, text='Comandos')
+button_frame.pack(fill='both', expand=True, pady=10, padx=10)
+
+add_button = Button(button_frame, text='Adicionar Livro')
+add_button.grid(row=0, column=0, padx=10, pady=10)
+
+update_button = Button(button_frame, text='Atualizar Livro')
+update_button.grid(row=0, column=1, padx=10, pady=10)
+
+remove_one_button = Button(button_frame, text='Deletar Livro')
+remove_one_button.grid(row=0, column=2, padx=10, pady=10)
+
+show_books_button = Button(button_frame, text='Mostar Livros')
+show_books_button.grid(row=0, column=3, padx=10, pady=10)
+
+show_authors_button = Button(button_frame, text='Mostrar Autores')
+show_authors_button.grid(row=0, column=4, padx=10, pady=10)
+
+show_publishing_company_button = Button(button_frame, text='Mostar Editoras')
+show_publishing_company_button.grid(row=0, column=5, padx=10, pady=10)
+
+# button = Button(button_frame, text='')
+# button.grid(row=0, column=0, padx=10, pady=10)
+
+# button = Button(button_frame, text='')
+# button.grid(row=0, column=0, padx=10, pady=10)
 
 root.mainloop()
